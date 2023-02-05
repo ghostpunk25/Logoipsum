752f7e04484fed4f94b1f57edf32e11ef41c8f5a
ghostpunk25 <105439703+ghostpunk25@users.noreply.github.com>
2023-02-05 13:21:41 +0200

    new

---
+++ Recediviz/index.5bb123ea.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".header__button"),t=document.querySelector(".header__items");e.addEventListener("click",(function(n){t.classList.toggle("menu-open"),e.classList.toggle("button-open")}));
+//# sourceMappingURL=index.5bb123ea.js.map
---
+++ Recediviz/index.d8208f4a.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".header__button"),t=document.querySelector(".header__items");e.addEventListener("click",(function(n){t.classList.toggle("menu-open"),e.classList.toggle("button-open")}))}();
+//# sourceMappingURL=index.d8208f4a.js.map
